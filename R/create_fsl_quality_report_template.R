81cf75cd197442b95367b78659dbe3a27f2ddba7
Quentin Villotta <quentin.villotta@gmail.com>
2025-07-01 12:03:02 +0300

    Try fix bug template creation .Rprofile via Rstudio User Interface

--- R/create_fsl_quality_report_template.R
+++ R/create_fsl_quality_report_template.R
@@ -11,15 +11,34 @@
 #' \dontrun{
 #' create_fsl_quality_report_template("path/to/folder")
 #' }
+#' Create a project folder for fsl quality report
+#'
+#' @param folder_path Path for the new folder
+#' @param ... Extra information collected from the RStudio wizard
+#'
+#' @return Folder with a copy of the fsl_quality_report
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' create_fsl_quality_report_template("path/to/folder")
+#' }
 create_fsl_quality_report_template <- function(folder_path, ...) {
   from <- system.file("fsl_quality_report", package = "impactR4PHU")
   fs::dir_copy(from, folder_path, overwrite = FALSE)
-  orig <- file.path(folder_path, "fsl_quality_report", "Rprofile.R")
-  dest <- file.path(folder_path, "fsl_quality_report", ".Rprofile")
-  if (fs::file_exists(orig)) {
-    fs::file_move(orig, dest)
+  # Try both possible locations for Rprofile.R
+  orig1 <- file.path(folder_path, "fsl_quality_report", "Rprofile.R")
+  orig2 <- file.path(getwd(), "Rprofile.R")
+  dest1 <- file.path(folder_path, "fsl_quality_report", ".Rprofile")
+  dest2 <- file.path(getwd(), ".Rprofile")
+  if (fs::file_exists(orig1)) {
+    fs::file_move(orig1, dest1)
+  } else if (fs::file_exists(orig2)) {
+    fs::file_move(orig2, dest2)
   } else {
     stop("Expected activation file Rprofile.R not found in template")
   }
   invisible(folder_path)
 }
+
