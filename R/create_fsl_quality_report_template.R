1931a11fdcbd7fe3f66fdc3b9de440048dbf580f
Quentin Villotta <quentin.villotta@gmail.com>
2025-07-01 12:18:21 +0300

    Debug test template creation rstudio web interface A2

--- R/create_fsl_quality_report_template.R
+++ R/create_fsl_quality_report_template.R
@@ -28,15 +28,16 @@ create_fsl_quality_report_template <- function(folder_path, ...) {
   from <- system.file("fsl_quality_report", package = "impactR4PHU")
   fs::dir_copy(from, folder_path, overwrite = FALSE)
   # Try both possible locations for Rprofile.R
-  orig1 <- file.path(folder_path, "fsl_quality_report", "Rprofile.R")
-  orig2 <- file.path(getwd(), "Rprofile.R")
-  dest1 <- file.path(folder_path, "fsl_quality_report", ".Rprofile")
-  dest2 <- file.path(getwd(), ".Rprofile")
+  orig1 <- file.path("Rprofile.R")
+  # orig1 <- file.path(folder_path, "fsl_quality_report", "Rprofile.R")
+  # orig2 <- file.path(getwd(), "Rprofile.R")
+  dest1 <- file.path(".Rprofile")
+  # dest1 <- file.path(folder_path, "fsl_quality_report", ".Rprofile")
+  # dest2 <- file.path(getwd(), ".Rprofile")
   if (fs::file_exists(orig1)) {
     fs::file_move(orig1, dest1)
-  } else if (fs::file_exists(orig2)) {
-    fs::file_move(orig2, dest2)
   } else {
+    warning(paste0("Actual wd : ", getwd()))
     stop(paste0("Expected activation file Rprofile.R not found in template. folder_path=", folder_path))
   }
   invisible(folder_path)
